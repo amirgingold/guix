b9a0e1f578f1a7e4f7afc4ab17dbfa9f030b6ff0
amirgingold <93983491+amirgingold@users.noreply.github.com>
2022-03-06 18:32:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,18 @@
-is_vm () { dmesg | grep -q 'Hypervisor detected'; }
+#is_vm () { dmesg | grep -q 'Hypervisor detected'; }
 
-if is_vm; then
-  device=/dev/sda
-else
-  device=/dev/nvme0n1
-fi
+device=/dev/nvme0n1
 
 # Wiping
-if ! is_vm; then
-  sgdisk -z "$device"
-fi
+sgdisk -z "$device"
 
 # Partitioning
-if is_vm; then
-  boot_partition="$device"1
-  swap_partition="$device"2
-  root_partition="$device"3
-  
-  sfdisk "$device" << EOF
-,1G,83
-,10G,82
-,,
-EOF
-else
-  boot_partition="$device"p1
-  swap_partition="$device"p2
-  root_partition="$device"p3
+boot_partition="$device"p1
+swap_partition="$device"p2
+root_partition="$device"p3
 
-  sgdisk "$device" --new=1:0:+1G  --typecode=1:ef00
-  sgdisk "$device" --new=2:0:+10G --typecode=2:8200
-  sgdisk "$device" --new=3:0:0    --typecode=3:8304
-fi
+sgdisk "$device" --new=1:0:+1G  --typecode=1:ef00
+sgdisk "$device" --new=2:0:+10G --typecode=2:8200
+sgdisk "$device" --new=3:0:0    --typecode=3:8304
 
 # Formatting
 mkfs.fat -F32 "$boot_partition"
@@ -53,9 +35,5 @@ wget https://github.com/amirgingold/guix/raw/main/channels.scm --directory-prefi
 guix pull
 hash guix
 
-if is_vm; then
-  wget https://github.com/amirgingold/guix/raw/main/config-vm.scm --output-document=config.scm
-else
-  wget https://github.com/amirgingold/guix/raw/main/config.scm
-fi
+wget https://github.com/amirgingold/guix/raw/main/config.scm
 guix system init config.scm /mnt
