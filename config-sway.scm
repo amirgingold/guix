af8a372e64069f12b68c11597a5f0c4fb98efa3f
amirgingold <93983491+amirgingold@users.noreply.github.com>
2024-12-31 23:12:27 +0200

    Update config-sway.scm

--- config-sway.scm
+++ config-sway.scm
@@ -1,46 +1,76 @@
-;; This buffer is for text that is not saved, and for Lisp evaluation.
-;; To create a file, visit it with C-x C-f and enter text in its buffer.
+(define-module (daviwil systems base)
+  #:use-module (srfi srfi-1)
+  #:use-module (gnu)
+  #:use-module (gnu system)
+  #:use-module (gnu system nss)
+  #:use-module (gnu system setuid)
+  #:use-module (nongnu packages linux)
+  #:use-module (nongnu packages video)
+  #:use-module (nongnu system linux-initrd)
+  #:export (system-config))
 
-(use-modules (gnu)
-             (gnu system nss)
-             (gnu home)
-             (gnu home services)
-             (gnu home services pm)
-             (gnu home services gnupg)
-             (gnu home services mcron)
-             (gnu home services shells)
-             (gnu home services desktop)
-             (nongnu packages linux)
-	     (nongnu system linux-initrd))
+(use-service-modules guix admin sysctl pm nix avahi dbus cups desktop linux
+                     mcron networking xorg ssh docker audio virtualization)
 
-(use-service-modules desktop guix)
-(use-package-modules bootloaders certs gnuzilla emacs emacs-xyz version-control wm
-                     compression curl fonts freedesktop gimp glib gnome gnome-xyz
-                     gstreamer kde-frameworks linux music package-management
-                     password-utils pdf pulseaudio shellutils ssh syncthing video
-                     web-browsers wget wm xdisorg xorg emacs)
+(use-package-modules audio video nfs certs shells ssh linux bash emacs gnome
+                     networking wm fonts libusb cups freedesktop file-systems
+                     version-control package-management vim)
 
-(define user-name "me")
+(define-public os-config
+  (operating-system
+   (host-name "M")
+   (timezone "Asia/Jerusalem")
+   (locale "en_IL.utf8")
+   (locale-definitions
+    (list (locale-definition (source "en_US")
+			     (name "en_US.UTF-8"))
+	  (locale-definition (source "he_IL")
+			     (name "he_IL.UTF-8"))))
+   (keyboard-layout "us,il" #:options '("grp:alt_shift_toggle"))
 
-(define sway-config
-  (map (lambda (str)
-         (string-append str "\n"))
-       (list
-        "set $mod Mod4"
-        "include \"~/.config/sway/before-config\""
-        "bindsym $mod+space exec fuzzel -w 50 -x 8 -y 8 -r 3 -b 232635ff -t A6Accdff -s A6Accdff -S 232635ff -C c792eacc -m c792eacc -f \"JetBrains Mono:weight=light:size=10\" --no-fuzzy"
-        "exec mako --border-radius=2 --font=\"JetBrains Mono 8\" --max-visible=5 --outer-margin=5 --margin=3 --background=\"#1c1f26\" --border-color=\"#89AAEB\" --border-size=1 --default-timeout=7000"
-        "exec nm-applet --indicator"
-        "exec emacs"
-        "include \"~/.config/sway/after-config\"")))
+   ;; Use non-free Linux and firmware
+   (kernel linux)
+   (firmware (list linux-firmware))
+   (initrd microcode-initrd)
 
-;; Basic System Config
-;; Home Config -- Configure Sway
+   (bootloader
+    (bootloader-configuration
+     (bootloader grub-efi-bootloader)
+     (targets '("/boot"))
+     (keyboard-layout keyboard-layout)
+     (timeout 3)))
 
-(define home-config
-  (home-environment
+   (file-systems
+    (append (list
+	     (file-system
+	      (device (file-system-label "BOOT"))
+              (mount-point "/boot")
+              (type "vfat"))
+             (file-system
+              (device (file-system-label "ROOT"))
+              (mount-point "/")
+              (type "ext4"))
+             (file-system
+              (device (file-system-label "WORKSPACE"))
+            (mount-point "/mnt/W")
+            (type "ext4")))
+          %base-file-systems))
+ 
+   (users (cons (user-account
+		 (name "me")
+                 (group "users")
+                 (home-directory "/home/me")
+                 (supplementary-groups '("wheel"  ;; sudo
+                                         "netdev" ;; network devices
+                                         "kvm"
+                                         "tty"
+					 "seat"
+                                         "input"
+                                         "audio"    ;; control audio devices
+                                         "video"))) ;; control video devices
+                %base-user-accounts))
    (packages (list
-              sway
+	      sway
               swaybg
               swayidle
               swaylock
@@ -85,54 +115,68 @@
               unzip
               trash-cli))
 
-   (services (list
-              (service home-xdg-configuration-files-service-type
-                       `(("sway/config" ,(apply mixed-text-file (cons "sway-config" sway-config)))))))))
-
-(define os-config
-  (operating-system
-   (host-name "M")
-
-   (timezone "Asia/Jerusalem")
-   (locale "en_US.utf8")
-
-   (kernel linux)
-   (firmware (list linux-firmware))
-   (initrd microcode-initrd)
-
-   (bootloader (bootloader-configuration
-		(bootloader grub-efi-bootloader)
-                (targets '("/boot"))
-		(timeout 2)))
+(define sway-config
+  (map (lambda (str)
+         (string-append str "\n"))
+       (list
+        "set $mod Mod4"))
    
-   (file-systems
-    (append (list
-	     (file-system
-	      (device (file-system-label "BOOT"))
-              (mount-point "/boot")
-              (type "vfat"))
-             (file-system
-              (device (file-system-label "ROOT"))
-              (mount-point "/")
-              (type "ext4"))
-	     (file-system
-              (device (file-system-label "WORKSPACE"))
-              (mount-point "/mnt/W")
-              (type "ext4")))
-	    %base-file-systems))
+   ;; Configure only the services necessary to run the system
+   (services (append
+              (modify-services %base-services
+               (delete login-service-type)
+               (delete mingetty-service-type)
+               (delete console-font-service-type))
+              (list
+	       (service seatd-service-type)
+	       (service greetd-service-type
+			(greetd-configuration 
+			 (greeter-supplementary-groups (list "video" "input" "seat"))
+			 (terminals
+			  (list
+			   (greetd-terminal-configuration
+			    (terminal-vt "1")
+			    (terminal-switch #t)
+			    (default-session-command
+			     (greetd-wlgreet-sway-session
+			      (sway-configuration sway-config))))
+                           (greetd-terminal-configuration
+			    (terminal-vt "2"))
+			   (greetd-terminal-configuration
+			    (terminal-vt "3"))
+			   (greetd-terminal-configuration
+			    (terminal-vt "4"))
+			   (greetd-terminal-configuration
+			    (terminal-vt "5"))
+			   (greetd-terminal-configuration
+			    (terminal-vt "6"))))))
 
-   (users (cons (user-account
-		 (name user-name)
-                 (group "users")
-                 (supplementary-groups '("wheel" "netdev"
-                                         "audio" "video")))
-		%base-user-accounts))
+               ;; Basic desktop system services (copied from %desktop-services)
+               (service avahi-service-type)
+               (service udisks-service-type)
+               (service upower-service-type)
+               (service cups-pk-helper-service-type)
+               (service geoclue-service-type)
+               (service polkit-service-type)
+               (service dbus-root-service-type)
+               fontconfig-file-system-service ;; Manage the fontconfig cache
 
-    (services (append (list (service guix-home-service-type
-                                     `((,user-name ,home-config))))
-                      %desktop-services))
+               ;; Power and thermal management services
+               (service thermald-service-type)
+               (service tlp-service-type
+                        (tlp-configuration
+                         (cpu-boost-on-ac? #t)
+                         (wifi-pwr-on-bat? #t)))
 
-    ;; Allow resolution of '.local' host names with mDNS.
-    (name-service-switch %mdns-host-lookup-nss)))
+               ;; Sync system clock with time servers
+               (service ntp-service-type)
+
+   ;; Allow resolution of '.local' host names with mDNS
+   (name-service-switch %mdns-host-lookup-nss)))
 
 os-config
+
+
+
+
+	     
